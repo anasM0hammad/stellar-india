ec0fa3787a095bd9bb748481a79836e088a9ddd2
anasM0hammad <anas.1633.m@gmail.com>
2019-07-12 01:24:47 +0530

    removing owl carousel

--- services.php
+++ services.php
@@ -9,6 +9,359 @@
 <!-- START WORK FROM HERE -->
 
 
+<!-- Service Page-->
+<!-- Services Carousel -->
+<div class="container-fluid pt-3 pb-3">
+  <div class="container p-3">
+    <div class="row">
+      <div class="col-lg-5"></div>
+      <div class="col-lg-4">
+        <h2 class="mb-4" style="font-weight: bolder; color: #FF7F00">Our Services</h2>
+      </div>
+      <div class="col-lg-3"></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-10">
+      <div class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="./img/pic1.jpg" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/lowVoltage.jpeg" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/electrical.jpg" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/fireSystem.png" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/hvac.jpg" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/pic3.jpg" class="img-fluid" alt="pic1">
+          </div>
+          <div class="item">
+            <img src="img/hotel.jpg" class="img-fluid" alt="pic1">
+          </div>
+      </div>
+    </div>
+    <div class="col-lg-1"></div>
+  </div>
+
+  <br>
+<div class="container"></div>
+  <div class="row">
+    <div class="col-lg-5"></div>
+    <div class="col-lg-4">
+      <h2 class="mb-4" style="color: #FF7F00">Design And Approach</h2>
+    </div>
+    <div class="col-lg-3"></div>
+  </div>
+  <div class="row" style="padding: 0px 15px 0px 15px;">
+    <div class="col-lg-6 pt-1">
+        <p class="mt-2"> <b>Stellar India Consultants</b> provide its specialized consultancy services in Electrical, Low Voltage,
+            HVAC, Public Health Engineering (Plumbing, Water and Waste Water Treatment & Management) and
+            Fire Fighting System along with, environment engineering systems and allied fields.
+        </p>
+    </div>
+    <div class="col-lg-6 pt-1">
+        <p class="mt-2"> <b>Stellar India</b> has specialized skills, manpower and expertise in planning of all kinds of building
+            complexes like, Group Housing/ Residential, Hotels, Institutional, Industrial, Townships,
+            Academic, Multiplexes and Mall buildings.
+        </p>
+    </div>
+  </div>
+</div>
+<!-- Service Carousel Ends Here -->
+
+
+
+
+  <!-- Design And Approach Services -->
+
+<div class="container">
+  <!-- Service - 1 --->
+  <div class="row">
+    <div class="col-lg-6"></div>
+    <div class="col-lg-6">
+      <h3 style="color: #FF7F00">HVAC</h3>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-4">
+        <img src="img/services/pic1.jpg">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-6">
+          <ul>
+            <li>Central Chilled Water System with Water Cooled & Air-Cooled Chillers</li>
+              <li>Primary & Variable Secondary Chilled Water System</li>
+              <li>Primary Variable Chilled Water System</li>
+              <li>Variable Refrigerant Flow (VRF) System</li>
+              <li>Water Cooled & Air-Cooled Package Unit System</li>
+              <li>Ductable Split Unit System</li>
+              <li>Mechanical Ventilation system</li>
+              <li>Evaporative Cooling System</li>
+              <li>Smoke Evacuation System</li>
+              <li>Winter Heating</li>
+          </ul>
+    </div>
+  </div>
+  <br><br>
+  
+  <!-- Service - 2 --->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col">
+      <h3 style="color: #FF7F00">Electrical System</h3>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-7">
+          <ul>
+            <li>HT Metering, HT Breakers, HT Cabling & Earthing, Transformers
+                LT Breakers, LT Isolators & LT Panel.</li>
+            <li>Bus Duct, Rising Main, Mains & Sub Mains.</li>
+            <li>Electrical Network – Internal & External.</li>
+            <li>Solar Lighting System.</li>
+            <li>Emergency Power Generation System.</li>
+            <li>Un-interrupted Power Supply (UPS) System.</li>
+            <li>Captive Power System.</li>
+            <li>Solar PV System.</li>
+          </ul>
+    </div>
+    <div class="col-lg-4">
+        <img src="img/services/pic2.jpeg">
+    </div>
+  </div>
+  <br><br>
+
+  <!-- Service 3 -->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-5"></div>
+    <div class="col-lg-6">
+      <h3 style="color: #FF7F00">Low Voltage</h3>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-4">
+        <img src="img/services/pic3.jpeg">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-6">
+          <ul>
+            <li>Fire Detection & Alarm System.</li>
+            <li>Public Address System.</li>
+            <li>Telephone & Data Infrastructure.</li>
+            <li>Building Management System.</li>
+            <li>Security System.</li>
+          </ul>
+    </div>
+  </div> <br><br>
+
+  <!-- Service - 4 --->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col">
+      <h3 style="color: #FF7F00">PUBLIC HEALTH ENGINEERING & FIRE FIGHTING SYSTEM</h3>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-7">
+          <ul>
+            <li>Pumping & Hydro-pneumatic Pumping System</li>
+            <li>Hot & Cold-Water Distribution System</li>
+            <li>Centralized Steam Boilers & Hot Water Generation</li>
+            <li>Steam Supply & Condensate Recovery</li>
+            <li>Heat Recovery Through Heat Pumps</li>
+            <li>Water & Waste Water Treatment</li>
+            <li>Sewage Treatment Plant, Tertiary Treatment & Re-Cycling</li>
+            <li>Rainwater Harvesting</li>
+            <li>Solar Hot Water & Heat Recovery System</li>
+            <li>Fire Fighting System Including Pumping, Wet Risers, Sprinklers & Water Curtain System.</li>
+          </ul>
+    </div>
+    <div class="col-lg-4">
+        <img src="img/services/pic4.jpeg">
+    </div>
+  </div>
+ 
+</div>
+<br><hr><br><br>
+
+
+
+
+
+
+<!-- Scope of Services - MEP Works--> 
+<div class="container">
+
+  <div class="row">
+    <div class="col-lg-3"></div>
+    <div class="col-lg-9">
+      <h1 style="color: #FF7F00">Scope Of Services - MEP Works</h1>
+    </div>
+  </div><br><br>
+
+  <!-- Service - 1 --->
+  <div class="row">
+    <div class="col-lg-6"></div>
+    <div class="col-lg-6">
+      <h3 style="color: #FF7F00">Concept Design Stage (CDS)</h3>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-4">
+        <img src="img/services/pic1.jpg">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-6">
+          <ul>
+              <li>Design Reports.</li>
+              <li>Space Planning.</li>
+              <li>Schematic Design (SLD).</li>
+          </ul>
+    </div>
+  </div>
+  <br><br><br>
+  
+  <!-- Service - 2 --->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col">
+      <h3 style="color: #FF7F00">Municipal/Fire Submission Stage (SDS)</h3>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-7">
+          <ul>
+              <li>Drawings for Submission.</li>
+              <li>Necessary Calculations like Electrical Loads, Ventilation Calculations,
+                    Pressurization Calculations and Hydraulic Calculations etc.</li>
+              <li>Necessary Documents like Fire Form etc.</li>
+          </ul>
+    </div>
+    <div class="col-lg-4">
+        <img src="img/services/pic2.jpeg">
+    </div>
+  </div>
+  <br><br><br>
+
+  <!-- Service 3 -->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-5"></div>
+    <div class="col-lg-6">
+      <h3 style="color: #FF7F00">Design Development Stage (DDS)</h3>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-4">
+        <img src="img/services/pic3.jpeg">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-6">
+          <ul>
+              <li>Working Drawings.</li>
+              <li>Detailed Drawings.</li>
+              <li>MEP Coordinated Plans.</li>
+          </ul>
+    </div>
+  </div>
+  <br><br><br>
+
+  <!-- Service - 4 --->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col">
+      <h3 style="color: #FF7F00">Tender Document Stage (TDS)</h3>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-7">
+          <ul>
+            <li>Tender Drawings.</li>
+            <li>Tender Documents Including (GCC, SCC, TS, BOQ)</li>
+            <li>Detailed Estimates.</li>
+            <li>GFC (Good for Making Shop Drawings)</li>
+          </ul>
+    </div>
+    <div class="col-lg-4">
+        <img src="img/services/pic4.jpeg">
+    </div>
+  </div>
+  <br><br><br>
+
+
+    <!-- service 5 -->
+    <div class="row">
+    <div class="col-lg-6"></div>
+    <div class="col-lg-6">
+      <h3 style="color: #FF7F00">Construction Work (at site)</h3>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-4">
+        <img src="img/services/pic5.jpeg">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-6">
+          <ul>
+              <li>Invitation of Bids.</li>
+              <li>Evaluation of Bids.</li>
+              <li>Assistance in award of work (if required).</li>
+              <li>Approval of shop drawings.</li>
+              <li>Periodic supervision/inspection of work at site.</li>
+          </ul>
+    </div>
+  </div>
+  <br><br><br>
+  
+  <!-- Service - 6 --->
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col">
+      <h3 style="color: #FF7F00">Green Building Certification</h3>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-7">
+          <ul>
+              <li>Coordination & Implementation in design requirements for Green Building
+              Certification as per consultant’s Matrix.</li>
+          </ul>
+    </div>
+    <div class="col-lg-4">
+        <img src="img/services/pic6.jpeg">
+    </div>
+  </div>
+ 
+</div>
+<!-- Cards : Scope of Services ENDS -->
+<br><br>
+
+
+
 
 
 <!-- YOUR WORK SHOULD BE END HERE -->
